cc3d6734adbcb2184258443a5a4fb962655c0e15
Your Name <you@example.com>
2019-03-18 10:34:03 +0700

    second commit

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 app.listen(3000, (req, res) => {
-    console.log('Server is conneting ....')
+    console.log('Server is conneting in port 3000....')
 })
 
 ///Mongoose connect
